8cc1c048b7cd1a739783b063c9e52b9a1412b482
jagcho <jagadishchodry@gmail.com>
2022-06-21 22:04:42 +0530

    updated deleteblog

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -7,7 +7,40 @@ const createBlog = async function(req,res){
         res.status(400).send({status:false, msg:"author is required"})
     }
     const createData = await BlogModel.create(data)
+    res.status(400).send({msg:createData})
+}
+
+
+const deleteblog1 = async function(req,res){
+        try {  
+      let blogId=req.params.blogId
+      if (!blogId) {
+        return res.status(404).send("No such blog exists");
+      }
+      let updatedblog = await BlogModel.findByIdAndUpdate({ _id: blogId },{isDeleted:true},{new:true});
+      res.status(200).send({ status:true, data: updatedblog });
+    }
+    catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
+
+    const deleteblog2 = async function(req,res){
+        try {  
+      let blogId=req.query.blogId
+      if (!blogId) {
+        return res.status(404).send("No such blog exists");
+      }
+      let updatedblog = await BlogModel.findByIdAndUpdate({ _id: blogId },{isDeleted:true},{new:true});
+      res.status(200).send({ status:true, data: updatedblog });
+    }
+    catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
 }
 
 
 module.exports.createBlog = createBlog
+module.exports.deleteblog1 = deleteblog1
+module.exports.deleteblog2 = deleteblog2
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,7 +5,8 @@ const BlogController= require("../controllers/blogController")
 
 router.post('/authors',AuthorController.createAuthor)
 router.post('/blogs',BlogController.createBlog)
-
+router.delete('/blogs/:blogId',BlogController.deleteblog1)
+router.delete('/blogs?queryParams',BlogController.deleteblog2)
 
 
 module.exports = router;
