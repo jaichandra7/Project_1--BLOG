7a0134333d88f71b3ef5354bc775ca80be348e69
vinayrathore45 <vinayrathore91190@gmail.com>
2022-06-21 22:15:18 +0530

    no changes

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -7,6 +7,8 @@ const createBlog = async function(req,res){
         res.status(400).send({status:false, msg:"author is required"})
     }
     const createData = await BlogModel.create(data)
+    //update = await BlogModel.findOneAndUpdate({},{},{})
+    res.status(201).send({msg:createData})
 }
 
 
