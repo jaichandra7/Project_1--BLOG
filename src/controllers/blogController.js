5a5a36540532633498cbbeb383c34368135e61e0
jagcho <jagadishchodry@gmail.com>
2022-06-22 10:50:39 +0530

    completed deleted api

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,6 +1,7 @@
 const BlogModel = require('../models/blogModel')
 
 const createBlog = async function(req,res){
+  try{
     let data = req.body
     const auth = data.authorId
     if(!auth){
@@ -8,30 +9,38 @@ const createBlog = async function(req,res){
     }
     const createData = await BlogModel.create(data)
     res.status(400).send({msg:createData})
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+}
 }
 
 
 const deleteblog1 = async function(req,res){
         try {  
-      let blogId=req.params.blogId
+      let blogId=req.params._id
       if (!blogId) {
         return res.status(404).send("No such blog exists");
       }
-      let updatedblog = await BlogModel.findByIdAndUpdate({ _id: blogId },{isDeleted:true},{new:true});
+      let updatedblog = await BlogModel.findByIdAndUpdate({_id:blogId },{isDeleted:true},{new:true});
       res.status(200).send({ status:true, data: updatedblog });
     }
     catch (err) {
         console.log("This is the error :", err.message)
         res.status(500).send({ msg: "Error", error: err.message })
     }
-
-    const deleteblog2 = async function(req,res){
+}
+ const deleteblog2 = async function(req,res){
         try {  
-      let blogId=req.query.blogId
-      if (!blogId) {
-        return res.status(404).send("No such blog exists");
-      }
-      let updatedblog = await BlogModel.findByIdAndUpdate({ _id: blogId },{isDeleted:true},{new:true});
+      let authorId=req.query.authorId
+      let category=req.query.category
+      //console.log(authorId)
+      let tags=req.query.tags
+      let subCategory=req.query.subCategory
+      // let updatedblog = await BlogModel.find({$or:[{authorId:authorId},{category:category},{tags:tags},{subCategory:subCategory}]}).updateMany({$set:{isDeleted:true}},{new:true});
+      let updatedblog = await BlogModel.findOneAndUpdate({$or:[{authorId:authorId},{category:category},{tags:tags},{subCategory:subCategory}] },{$set:{isDeleted:true}},{new:true});
+      console.log(updatedblog)
       res.status(200).send({ status:true, data: updatedblog });
     }
     catch (err) {
@@ -40,7 +49,6 @@ const deleteblog1 = async function(req,res){
     }
 }
 
-
-module.exports.createBlog = createBlog
+module.exports.createBlog =  createBlog
 module.exports.deleteblog1 = deleteblog1
 module.exports.deleteblog2 = deleteblog2
